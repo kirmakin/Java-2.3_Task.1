cf595131a12afa953f1e5192377590495856b748
Kirill <kiruxastar@gmail.com>
2020-09-07 21:00:31 +0300

    Tests upd

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class BonusServiceTest {
 
     @org.junit.jupiter.api.Test
-    void calculate() {
+    void shouldCalculateRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
         long amount = 1000_60;
         boolean registered = true;
@@ -20,18 +20,6 @@ class BonusServiceTest {
         BonusService service = new BonusService();
         long amount = 1000_60;
         boolean registered = false;
-        long expected = 300;
-
-        long actual = service.calculate(amount, registered);
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void shouldCalculateNotRegistered() {
-        BonusService service = new BonusService();
-        long amount = 1000_60;
-        boolean registered = false;
         long expected = 10;
 
         long actual = service.calculate(amount, registered);
@@ -39,17 +27,6 @@ class BonusServiceTest {
         assertEquals(expected, actual);
     }
 
-    @Test
-    void shouldCalculateRegisteredAndUnderlimit() {
-        BonusService service = new BonusService();
-        long amount = 1000_60;
-        boolean registered = true;
-        long expected = 600;
-
-        long actual = service.calculate(amount, registered);
-
-        assertEquals(expected, actual);
-    }
     @Test
     void shouldCalculateRegisteredAndBonusIs500() {
         BonusService service = new BonusService();
